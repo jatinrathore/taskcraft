ee6cff780cea8736d68acfc482b70a4a0a293b0a
Jatin <rathorejatin168@gmail.com>
2024-04-28 23:51:18 +0530

    fix: bug fixed and remover extra files

--- src/components/SigninForm.tsx
+++ src/components/SigninForm.tsx
@@ -71,57 +71,59 @@ const SigninForm = () => {
     toast.success("User successfully logged in!");
   };
   return (
-    <div className="sp-grid grid grid-rows-1 grid-cols-1 lg:grid-cols-2 p-10 w-full md:w-10/12">
-      <div className="gif-box  hidden lg:block rounded-lg">
-        <img src="/assets/Sign-In-Animation.gif" />
-      </div>
-      <div className="sp-form-box  flex flex-col justify-center items-center">
-        <h2 className="text-2xl md:text-3xl font-bold mb-12">
-          Sign in to TaskCraft
-        </h2>
-        <form
-          className="flex flex-col w-80 md:w-96 gap-5"
-          onSubmit={handleSubmit}
-          noValidate
-        >
-          <div>
-            <label className="block text-gray-500 font-bold mb-1 pr-4">
-              Email
-            </label>
-            <input
-              type="email"
-              name="email"
-              value={formData.email}
-              onChange={handleChange}
-              className="appearance-none border-2 border-gray-200 rounded-xl w-full p-2 md:p-4 text-gray-700 leading-tight focus:outline-none focus:border-pink-200 hover:border-pink-200"
-            />
-            {errors.email && (
-              <p className="text-red-500 text-xs italic mt-1 ml-3">
-                {errors.email}
-              </p>
-            )}
-          </div>
-          <div>
-            <label className="block text-gray-500 font-bold mb-1 pr-4">
-              Password
-            </label>
-            <input
-              type="password"
-              name="password"
-              value={formData.password}
-              onChange={handleChange}
-              className="appearance-none border-2 border-gray-200 rounded-xl w-full p-2 md:p-4 text-gray-700 leading-tight focus:outline-none focus:border-pink-200 hover:border-pink-200"
-            />
-          </div>
-          <div className="btn-signup">
-            <button
-              className="shadow bg-custom-navy-blue focus:shadow-outline focus:outline-none text-white font-bold p-2 md:p-4 rounded-full w-full  mt-3 md:mt-5 hover:bg-custom-navy-blue-hover"
-              type="submit"
-            >
-              Sign in
-            </button>
-          </div>
-        </form>
+    <div className="flex justify-center">
+      <div className="sp-grid grid grid-rows-1 grid-cols-1 lg:grid-cols-2 p-10 w-full md:w-10/12">
+        <div className="gif-box  hidden lg:block rounded-lg">
+          <img src="/assets/Sign-In-Animation.gif" />
+        </div>
+        <div className="sp-form-box  flex flex-col justify-center items-center">
+          <h2 className="text-2xl md:text-3xl font-bold mb-12">
+            Sign in to TaskCraft
+          </h2>
+          <form
+            className="flex flex-col w-80 md:w-96 gap-5"
+            onSubmit={handleSubmit}
+            noValidate
+          >
+            <div>
+              <label className="block text-gray-500 font-bold mb-1 pr-4">
+                Email
+              </label>
+              <input
+                type="email"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+                className="appearance-none border-2 border-gray-200 rounded-xl w-full p-2 md:p-4 text-gray-700 leading-tight focus:outline-none focus:border-pink-200 hover:border-pink-200"
+              />
+              {errors.email && (
+                <p className="text-red-500 text-xs italic mt-1 ml-3">
+                  {errors.email}
+                </p>
+              )}
+            </div>
+            <div>
+              <label className="block text-gray-500 font-bold mb-1 pr-4">
+                Password
+              </label>
+              <input
+                type="password"
+                name="password"
+                value={formData.password}
+                onChange={handleChange}
+                className="appearance-none border-2 border-gray-200 rounded-xl w-full p-2 md:p-4 text-gray-700 leading-tight focus:outline-none focus:border-pink-200 hover:border-pink-200"
+              />
+            </div>
+            <div className="btn-signup">
+              <button
+                className="shadow bg-custom-navy-blue focus:shadow-outline focus:outline-none text-white font-bold p-2 md:p-4 rounded-full w-full  mt-3 md:mt-5 hover:bg-custom-navy-blue-hover"
+                type="submit"
+              >
+                Sign in
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   );
--- src/components/SignupForm.tsx
+++ src/components/SignupForm.tsx
@@ -66,79 +66,81 @@ const SignupForm = () => {
   };
 
   return (
-    <div className="sp-grid grid grid-rows-1 grid-cols-1 lg:grid-cols-2 p-10 w-full md:w-10/12">
-      <div className="gif-box hidden lg:flex rounded-lg">
-        <img src="/assets/Sign-Up-Animation.gif" />
-      </div>
-      <div className="sp-form-box flex flex-col justify-center items-center">
-        <h2 className="text-2xl md:text-3xl font-bold mb-8 md:mb-12">
-          Sign up to TaskCraft
-        </h2>
-        <form
-          className="flex flex-col w-80 md:w-96 gap-5"
-          onSubmit={handleSubmit}
-          noValidate
-        >
-          <div>
-            <label className="block text-gray-500 font-bold mb-1 pr-4">
-              Name
-            </label>
-            <input
-              type="text"
-              name="name"
-              value={formData.name}
-              onChange={handleChange}
-              className="appearance-none border-2 border-gray-200 rounded-xl w-full p-2 md:p-4 text-gray-700 leading-tight focus:outline-none focus:border-pink-200 hover:border-pink-200"
-            />
-            {errors.name && (
-              <p className="text-red-500 text-xs italic mt-1 ml-3">
-                {errors.name}
-              </p>
-            )}
-          </div>
-          <div>
-            <label className="block text-gray-500 font-bold mb-1 pr-4">
-              Email
-            </label>
-            <input
-              type="email"
-              name="email"
-              value={formData.email}
-              onChange={handleChange}
-              className="appearance-none border-2 border-gray-200 rounded-xl w-full p-2 md:p-4 text-gray-700 leading-tight focus:outline-none focus:border-pink-200 hover:border-pink-200"
-            />
-            {errors.email && (
-              <p className="text-red-500 text-xs italic mt-1 ml-3">
-                {errors.email}
-              </p>
-            )}
-          </div>
-          <div>
-            <label className="block text-gray-500 font-bold mb-1 pr-4">
-              Password
-            </label>
-            <input
-              type="password"
-              name="password"
-              value={formData.password}
-              onChange={handleChange}
-              className="appearance-none border-2 border-gray-200 rounded-xl w-full p-2 md:p-4 text-gray-700 leading-tight focus:outline-none focus:border-pink-200 hover:border-pink-200"
-            />
-            {errors.password && (
-              <p className="text-red-500 text-xs italic mt-1 ml-3">
-                {errors.password}
-              </p>
-            )}
-          </div>
-          <div className="btn-signup">
-            <button
-              className="shadow bg-custom-navy-blue focus:shadow-outline focus:outline-none text-white font-bold p-2 md:p-4 rounded-full w-full mt-3 md:mt-5 hover:bg-custom-navy-blue-hover"
-              type="submit"
-            >
-              Create Account
-            </button>
-          </div>
-        </form>
+    <div className="flex justify-center">
+      <div className="sp-grid grid grid-rows-1 grid-cols-1 lg:grid-cols-2 p-10 w-full md:w-10/12">
+        <div className="gif-box hidden lg:flex rounded-lg">
+          <img src="/assets/Sign-Up-Animation.gif" />
+        </div>
+        <div className="sp-form-box flex flex-col justify-center items-center">
+          <h2 className="text-2xl md:text-3xl font-bold mb-8 md:mb-12">
+            Sign up to TaskCraft
+          </h2>
+          <form
+            className="flex flex-col w-80 md:w-96 gap-5"
+            onSubmit={handleSubmit}
+            noValidate
+          >
+            <div>
+              <label className="block text-gray-500 font-bold mb-1 pr-4">
+                Name
+              </label>
+              <input
+                type="text"
+                name="name"
+                value={formData.name}
+                onChange={handleChange}
+                className="appearance-none border-2 border-gray-200 rounded-xl w-full p-2 md:p-4 text-gray-700 leading-tight focus:outline-none focus:border-pink-200 hover:border-pink-200"
+              />
+              {errors.name && (
+                <p className="text-red-500 text-xs italic mt-1 ml-3">
+                  {errors.name}
+                </p>
+              )}
+            </div>
+            <div>
+              <label className="block text-gray-500 font-bold mb-1 pr-4">
+                Email
+              </label>
+              <input
+                type="email"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+                className="appearance-none border-2 border-gray-200 rounded-xl w-full p-2 md:p-4 text-gray-700 leading-tight focus:outline-none focus:border-pink-200 hover:border-pink-200"
+              />
+              {errors.email && (
+                <p className="text-red-500 text-xs italic mt-1 ml-3">
+                  {errors.email}
+                </p>
+              )}
+            </div>
+            <div>
+              <label className="block text-gray-500 font-bold mb-1 pr-4">
+                Password
+              </label>
+              <input
+                type="password"
+                name="password"
+                value={formData.password}
+                onChange={handleChange}
+                className="appearance-none border-2 border-gray-200 rounded-xl w-full p-2 md:p-4 text-gray-700 leading-tight focus:outline-none focus:border-pink-200 hover:border-pink-200"
+              />
+              {errors.password && (
+                <p className="text-red-500 text-xs italic mt-1 ml-3">
+                  {errors.password}
+                </p>
+              )}
+            </div>
+            <div className="btn-signup">
+              <button
+                className="shadow bg-custom-navy-blue focus:shadow-outline focus:outline-none text-white font-bold p-2 md:p-4 rounded-full w-full mt-3 md:mt-5 hover:bg-custom-navy-blue-hover"
+                type="submit"
+              >
+                Create Account
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   );
