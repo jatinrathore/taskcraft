fedff0506a666213a7c92649dce8ec102bd179dd
Jatin <rathorejatin168@gmail.com>
2024-04-28 19:48:57 +0530

    fix: route changes

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,20 +1,9 @@
 import { createBrowserRouter } from "react-router-dom";
-import AppLayout from "./pages/AppLayout";
-import SignupForm from "./components/SignupForm";
-import SigninForm from "./components/SigninForm";
 import Dashboard from "./pages/Dashboard";
 import SignupPage from "./pages/SignupPage";
 import SigninPage from "./pages/SigninPage";
 
 const router = createBrowserRouter([
-  // {
-  //   path: "",
-  //   element: <AppLayout />,
-  //   children: [
-  //     { index: true, element: <SignupForm /> },
-  //     { path: "/sign-in", element: <SigninForm /> },
-  //   ],
-  // },
   {
     path: "/",
     index: true,
